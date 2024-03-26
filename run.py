df03b42bf213eab06e4daf1599fc816344ffa010
Python4k <python4k@vk.com>
2024-03-26 22:27:49 +0300

    Refactoring project in MVC pattern (it's not working right now)

--- run.py
+++ run.py
@@ -1,35 +1,8 @@
-from flask import Flask, render_template
+from blog import create_app, db
 
-app = Flask(__name__)
-
-
-@app.route('/')
-def index():
-    return render_template('index.html', title='Главная')
-
-@app.route('/html_page')
-def html_page():
-    return render_template('html_page.html', title='HTML')
-
-@app.route('/css_page')
-def css_page():
-    return render_template('css_page.html', title='CSS')
-
-@app.route('/js_page')
-def js_page():
-    return render_template('js_page.html', title='JAVASCRIPT')
-
-@app.route('/python_page')
-def python_page():
-    return render_template('python_page.html', title='PYTHON')
-
-@app.route('/flask_page')
-def flask_page():
-    return render_template('flask_page.html', title='FLASK')
-
-@app.route('/django_page')
-def django_page():
-    return render_template('django_page.html', title='DJANGO')
+app = create_app()
 
 if __name__ == "__main__":
-    app.run(debug=True, port=8000)
+    with app.app_context():
+        db.create_all()
+        app.run(debug=True)
